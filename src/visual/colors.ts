13c3639850c8af31a49ff7e1518d93b6b8313664
Gosch <gosch@gosch.io>
2021-08-10 01:25:09 -0300

    Implements colors

--- src/visual/colors.ts
+++ src/visual/colors.ts
@@ -1,1 +1,6 @@
-export default {};
+export default {
+  primary: '#DBA14B',
+  secondary: '#234A6B',
+  black: '#171717',
+  white: '#F2F2F2',
+};
--- src/visual/styles/GlobalStyle.tsx
+++ src/visual/styles/GlobalStyle.tsx
@@ -1,24 +1,29 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  body, html {
-    min-height: 100vh;
-  }
+  ${({ theme }) => css`
+    body,
+    html {
+      min-height: 100vh;
+      background-color: ${theme.colors.black};
+      color: ${theme.colors.white};
+    }
 
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-weight: normal;
-  }
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-weight: normal;
+    }
 
-  button {
-    cursor: pointer;
-    border: none;
-  }
+    button {
+      cursor: pointer;
+      border: none;
+    }
 
-  img {
-    max-width: 100%;
-    display: block;
-  }
+    img {
+      max-width: 100%;
+      display: block;
+    }
+  `}
 `;
